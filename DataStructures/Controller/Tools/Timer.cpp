7f2ed6376546402f168f7244bf50e71dfcbc3a0a
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-01-31 11:58:41 -0700

    I completed the timer.hpp file

---
+++ DataStructures/Controller/Tools/Timer.cpp
@@ -0,0 +1,21 @@
+//
+//  Timer.cpp
+//  DataStructures
+//
+//  Created by Burr, Alan on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    execution
+}
