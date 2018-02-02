bb28cc415cbcaa079cf932a522b0ec0c7e8ca654
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-02-02 11:51:46 -0700

    I added a timer and a method to sort the crime data

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -30,3 +30,30 @@ void Controller :: start()
     codeTimer.stopTimer();
     codeTimer.displayInformation();
 }
+
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
+    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/abur9000/Documents/CS 2420/DataStructures/DataStructures/Data/crime.csv");
+    
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 1; index < myData.size(); index++)
+    {
+        if (myData [minIndex] > myData [index])
+        {
+            minIndex = index;
+        }
+        
+        if (myData [maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
+    }
+    searchTimer.stopTimer();
+    cout << "The smallest Crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The largest Crime stat is at " << maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
+}
