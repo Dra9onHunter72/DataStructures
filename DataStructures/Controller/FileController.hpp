b80478234ec0d19014108ababa5674a93e401109
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-02-20 13:31:47 -0700

    I made it so that it will test the LinkedList.hpp file

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -15,6 +15,43 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include "/Users/abur9000/Documents/CS 2420/DataStructures/DataStructures/Model/Structure/LinkedList.hpp"
+
+static LinkedList<CrimeData> readDataToList(string filename);
+LinkedList<CrimeData> FileController :: readDataToList(string filename)
+{
+    LinkedList<CrimeData> crimes;
+    
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(fileName);
+    
+    //If the file exists at that path.
+    if (dataFile.is_open())
+    {
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '\r');
+            if (rowCount != 0)
+            {
+                if (currentCSVLine.length() ! = 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimes.add(row)
+                }
+            }
+            rowCount++
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimes;
+}
 
 using namespace std;
 
---
+++ DataStructures/Testers/LinkedListTester.h
@@ -0,0 +1,25 @@
+//
+//  LinkedListTester.h
+//  DataStructures
+//
+//  Created by Burr, Alan on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedListTester_h
+#define LinkedListTester_h
+
+#include "../Model/Structure/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Data/CrimeData.hpp"
+#include <iostream>
+
+class LinkedListTester
+{
+public:
+    void testListBasics();
+    void testListWithData();
+};
+
+#endif /* LinkedListTester_h */
