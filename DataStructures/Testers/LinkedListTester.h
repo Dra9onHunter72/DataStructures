dd79025050533142d6f00df5cb4f8678289557e2
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-02-20 13:45:14 -0700

    I added to but not finished the LinkedListTester.hpp

--- DataStructures/Testers/LinkedListTester.h
+++ DataStructures/Testers/LinkedListTester.h
@@ -22,4 +22,15 @@ public:
     void testListWithData();
 };
 
+void LinkedListTester :: testListBasics()
+{
+    LinkedList<int> numbers;
+    numbers.add(9);
+    cout << numbers.getFront()->getData() << " Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " is at the end of the list and should be 9" << endl;
+    
+    numbers.add(0)
+    cout << numbers.getFront()->getData() << " IS is at the front of the list and should be 9" <<
+}
+
 #endif /* LinkedListTester_h */
