2524de488bc280b4eef3bc9d5127bc01390ca3f4
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-03-13 13:30:38 -0600

    I added to testingStacksAndQueues.cpp

--- DataStructures/Testers/TestingStacksAndQueues.cpp
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -40,3 +40,52 @@ listTimer.stopTimer();
 cout << "The time it took to work with Stack" << endl;
 stackTimer.displayInformation();
 cout << "the time it took to work with List"
+listTimer.displayInformation();
+
+cout << "What is left in the Stack" << endl;
+LinkedList<int> * listVersion = &numberStack;
+int stackSize = listVersion->getSize();
+cout << "Stack size is: " << stackSize << endl;
+for (int index = 0; index < listSize; index++)
+     {
+         cout << numberList.getFromIndex(index) << "\t";
+     }
+
+void TestStacksAndQueues :: stackVersusQueue()
+{
+    vector<CrimeData> data = FileController :: readCrimeDataToVector("path to file");
+    
+    Stack<CrimeData> crimeStack;
+    Queue<CrimeData> crimeQueue;
+    
+    
+    for (int index = 0; index < 100; index++)
+    {
+        CrimeData temp = data.at(index);
+        crimeStack.push(temp);
+        crimeQueue.enqueue(temp);
+    }
+    
+    Queue<int> queue;
+    Stack<int> stack;
+    Queue<int> queueFromStack;
+    Stack<int> stackFromQueue;
+    
+    for (int index = 0; index < 10; index++)
+    {
+        queue.enqueue(index);
+        stack.push(index);
+    }
+    
+    for (int index = 0; index < 10; index++)
+    {
+        cout << index << endl;
+        cout << "Queue results: " << queue.dequeue() << endl;
+        cout << "Stack results: " << stack.pop() << endl;
+    }
+    
+    for (int index = 0; index < 10; index++)
+    {
+        queue.enqueue(index);
+    }
+}
