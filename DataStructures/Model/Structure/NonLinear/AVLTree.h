b580c1f079149317b082b29089e0f2d58b27177e
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-04-17 12:04:57 -0600

    I added the AVLTree.h class and added the private and public class to It

---
+++ DataStructures/Model/Structure/NonLinear/AVLTree.h
@@ -0,0 +1,50 @@
+//
+//  AVLTree.h
+//  DataStructures
+//
+//  Created by Burr, Alan on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_h
+#define AVLTree_h
+
+#include "BinarySearchTree.h"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    
+    BinaryTreeNode<Type> * leftRotation (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree (BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type>* parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = this->calculateHeight(node->getLeftNode());
+    int rightHeight = this->calculateHeight(node->getRightNode());
+    balance = leftHeight - rightHeight;
+    return balance;
+}
+
+template <class Type>
+
+#endif /* AVLTree_h */
