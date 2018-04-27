aa36c691dbbf6f60f1eb887685165169476c8ec6
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-04-27 12:50:28 -0600

    I added the Hashtable.h to my project

---
+++ DataStructures/Model/Structure/NonLinear/Hashtable.h
@@ -0,0 +1,93 @@
+//
+//  Hashtable.h
+//  DataStructures
+//
+//  Created by Burr, Alan on 4/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Hashtable_h
+#define Hashtable_h
+
+#include "/Users/abur9000/Documents/CS 2420/DataStructures/DataStructures/Model/Nodes/HashNode.h"
+
+template <class Type>
+class HashTable
+{
+private:
+    HashNode<Type> ** internalStorage;
+    long size;
+    long capacity;
+    double loadFactor;
+    
+    long getNextPrime();
+    bool isPrime(long current);
+    void resize();
+    long findPosition(HashNode<Type>* insertedNode);
+    long handleCollision(HashNode<Type>* insertedNode, long index);
+    
+public:
+    Hashtable();
+    ~Hashtable();
+    
+    void insert(Type data);
+    long getSize();
+};
+
+template <class Type>
+Hashtable<Type> :: Hashtable()
+{
+    capacity = 101;
+    internalStorage = new HashNode<Type>*[capacity];
+    size = 0;
+    loadFactor = .80;
+}
+
+template <class Type>
+Hashtable<Type> :: ~Hashtable()
+{
+    delete [] internalStorage;
+}
+
+template <class Type>
+bool Hashtable<Type> :: isPrime(long current)
+{
+    return false;
+}
+
+template <class Type>
+long Hashtable<Type> :: findPosition(HashNode<Type> * insert)
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: handleCollision(HashNode<Type> * current, long index)
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: getSize()
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: getNextPrime()
+{
+    return -1;
+}
+
+template <class Type>
+void Hashtable<Type> :: resize()
+{
+    
+}
+
+template <class Type>
+void Hashtable<Type> :: insert(Type value)
+{
+    
+}
+#endif /* Hashtable_h */
